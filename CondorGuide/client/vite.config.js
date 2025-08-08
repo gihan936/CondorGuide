99d9d0c01ef3d206c2306136b5937800a572a74d
gihan936 <gihankodikara93@gmail.com>
2025-08-08 10:37:36 -0400

    Update vite.config.js file

--- CondorGuide/client/vite.config.js
+++ CondorGuide/client/vite.config.js
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite';
 
-
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: '${import.meta.env.VITE_API_BASE_URL}',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
 });
