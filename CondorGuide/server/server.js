40c619b36a607a25af82c416b7786a59845312c5
gihan936 <gihankodikara93@gmail.com>
2025-08-11 14:13:56 -0400

    Fix security issue

--- CondorGuide/server/server.js
+++ CondorGuide/server/server.js
@@ -25,7 +25,7 @@ const app = express();
 
 app.use(cors({
   origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['*'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   optionsSuccessStatus: 204,
 }));
